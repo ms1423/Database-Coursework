f49c9ae676efb0b11d49a04382b3369d851f5897
ms1423 <49311258+ms1423@users.noreply.github.com>
2019-11-19 10:21:12 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+  echo "Hello NMSL";
+
+
+?>
+
+<!doctype html>
+
+<html>
+
+<head>
+    <title> NMSL  PHP</title>
+</head>
+
+<body>
+    <h1> <?php echo "PHP NMSL";?> </h1>
+    <p> clot </p>
+    <p> supreme</p>
+    <p>palace</p>
+
+</body>
+
+</html>
